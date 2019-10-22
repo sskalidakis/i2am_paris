6466b4b71813c027846bbee55499b46f9f8c058a
epu_temp_steve <sskalidakis@epu.ntua.gr>
2019-10-22 17:27:48 +0300

    -overview and comparative assessment completed

--- i2amparis_main/urls.py
+++ i2amparis_main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('main', views.landing_page, name='landing_page'),
+    path('overview_comparative_assessment_doc', views.overview_comparative_assessment_doc, name='overview_comparative_assessment_doc'),
 ]
--- i2amparis_main/views.py
+++ i2amparis_main/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 
 def landing_page(request):
-    print ('hellooooo!!!')
+    print ('Landing page')
     return render(request, 'i2amparis_main/landing_page.html')
+
+def overview_comparative_assessment_doc(request):
+    print('Overview Comparative Assessment')
+    return render(request, 'i2amparis_main/overview_comparative_assessment_doc.html')
+
+
+    # return render(request, 'i2amparis_main/index.html')
