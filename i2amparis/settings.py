4dd4ad11f68829391a9c693ef4b87e84ae14fb4b
epu_temp_steve <sskalidakis@epu.ntua.gr>
2019-11-13 17:41:36 +0200

    -fix settings

--- i2amparis/settings.py
+++ i2amparis/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'i2amparis_main/static'),
+    # os.path.join(BASE_DIR, 'i2amparis_main/static'),
 
 ]
 
