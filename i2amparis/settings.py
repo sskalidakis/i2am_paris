d09bf25cc385e99a044e43d48503ffade5126e9a
epu_temp_steve <sskalidakis@epu.ntua.gr>
2019-11-13 17:36:11 +0200

    -newsettings static

--- i2amparis/settings.py
+++ i2amparis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%jq+tdb&qkl+4=wcl71hn&u+%8_$^mjp7mt6cr*u@l_-zjouf#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','paris-reinforce.epu.ntua.gr']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
