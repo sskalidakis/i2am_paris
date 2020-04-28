39895b9e5900169e77ca56e30ddbd280c768a068
epu_temp_steve <sskalidakis@epu.ntua.gr>
2020-04-28 17:40:55 +0300

    small changes

--- visualiser/views.py
+++ visualiser/views.py
@@ -1,9 +1,12 @@
+from json import JSONDecodeError
+
 from django.shortcuts import render
+
 
 from django.http import HttpResponse
 
 from visualiser.fake_data.fake_data import FAKE_DATA, COLUMNCHART_DATA, BAR_RANGE_CHART_DATA, BAR_HEATMAP_DATA, \
-    HEAT_MAP_DATA
+    HEAT_MAP_DATA, HEAT_MAP_CHART_DATA
 
 AM_CHARTS_COLOR_INDEX_LIST = {
     "light_blue": 0,
@@ -20,18 +23,61 @@ AM_CHARTS_COLOR_INDEX_LIST = {
     "cyan": 19,
 }
 
+AM_CHARTS_COLOR_CODES_LIST = {
+    "light_blue": "#539AD6",
+    "blue": "#3869D6",
+    "violet": "#7533D6",
+    "purple": "#7B2CA2",
+    "fuchsia": "#942A7C",
+    "red": "#AA1C28",
+    "ceramic": "#9c3421",
+    "light_brown": "#843E1F",
+    "mustard": "#c49d00",
+    "gold": "#e6ac00",
+    "light_green": "#5A992E",
+    "green": "#26671E",
+    "cyan": "#2c9e95",
+}
+
 AM_CHARTS_COLOR_HEATMAP_COUPLES = {
-    "blue_red": ["#539ad6", "#aa352f"],
-    "green_red": ["#74b377", "#bf333c"],
-    "brown_purple": ["#eeba95", "#74397a"],
-    "purple_orange": ["#e6c8ee", "#db7c3d"],
-    "cyan_green": ["#89c3c2", "#4b6822"],
-    "yellow_gold": ["#f7ecc2", "#f5b500"],
-    "beige_red": ["#f7dfd0", "#8d1915"],
+    "blue_red": ["#63a1db", "#a32f22"],
+    "green_red": ["#66bd7d", "#a32f22"],
+    "beige_purple": ["#f0d2bd", "#80308a"],
+    "purple_orange": ["#f5d1ff", "#db6b21"],
+    "cyan_green": ["#99c9c9", "#446614"],
+    "yellow_gold": ["#f7ecc2", "#dba200"],
+    "skin_red": ["#f7dfd0", "#8d1915"],
     "grey_darkblue": ["#eaecf7", "#1f3b5e"],
-    "lightblue_green": ["#bbe1ff", "#2e5c20"]
 
 }
+
+class XYZ_chart:
+    def __init__(self, request, x_axis_name, x_axis_title, x_axis_unit, y_axis_name, y_axis_title, y_axis_unit,
+                 z_axis_name, z_axis_title, z_axis_unit, chart_data, color_list, minmax_z_value, chart_type):
+        self.x_axis_name = x_axis_name
+        self.x_axis_title = x_axis_title
+        self.x_axis_unit = x_axis_unit
+        self.y_axis_name = y_axis_name
+        self.y_axis_title = y_axis_title
+        self.y_axis_unit = y_axis_unit
+        self.z_axis_name = z_axis_name
+        self.z_axis_title = z_axis_title
+        self.z_axis_unit = z_axis_unit
+        self.chart_data = chart_data
+        self.request = request
+        self.chart_type = chart_type
+        self.color_list = color_list
+        self.minmax_z_value = minmax_z_value
+        self.content = {'x_axis_title': self.x_axis_title, 'x_axis_unit': self.x_axis_unit,
+                        'x_axis_name': self.x_axis_name, 'y_axis_title': self.y_axis_title,
+                        'y_axis_unit': self.y_axis_unit, 'y_axis_name': self.y_axis_name,'z_axis_name': z_axis_name,
+                        'z_axis_title': z_axis_title, 'z_axis_unit': z_axis_unit, 'color_list': self.color_list,
+                        'minmax_z_value': self.minmax_z_value, 'chart_data': self.chart_data}
+
+    def show_chart(self):
+        if self.chart_type == 'heat_map_chart':
+            return render(self.request, 'visualiser/heat_map_chart.html',
+                          self.content)
 
 
 class XY_chart:
@@ -99,7 +145,7 @@ def show_line_chart(request):
     chart_3d = "false"
     min_max_y_value = [0, 2000]
 
-    color_list = define_color_list(color_list_request)
+    color_list = define_color_index_list(color_list_request)
 
     line_chart = XY_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_var_names, y_var_titles, y_var_units,
                           x_axis_type, y_axis_title, data, color_list, use_default_colors, chart_3d, min_max_y_value,
@@ -123,7 +169,7 @@ def show_column_chart(request):
     chart_3d = "true"
     min_max_y_value = [0, 2000]
 
-    color_list = define_color_list(color_list_request)
+    color_list = define_color_index_list(color_list_request)
     column_chart = XY_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_var_names, y_var_titles, y_var_units,
                             x_axis_type, y_axis_title, data, color_list, use_default_colors, chart_3d, min_max_y_value,
                             'column_chart')
@@ -145,7 +191,7 @@ def show_range_chart(request):
     chart_3d = "false"
     min_max_y_value = [0, 2000]
 
-    color_list = define_color_list(color_list_request)
+    color_list = define_color_index_list(color_list_request)
 
     range_chart = XY_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_var_names, y_var_titles, y_var_units,
                            x_axis_type, y_axis_title, data, color_list, use_default_colors, chart_3d, min_max_y_value,
@@ -188,7 +234,7 @@ def show_stacked_column_chart(request):
     use_default_colors = "false"
     chart_3d = "true"
     min_max_y_value = [0, 2000]
-    color_list = define_color_list(color_list_request)
+    color_list = define_color_index_list(color_list_request)
     stacked_column_chart = XY_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_var_names, y_var_titles, y_var_units,
                             x_axis_type, y_axis_title, data, color_list, use_default_colors, chart_3d, min_max_y_value,
                                     'stacked_column_chart')
@@ -196,7 +242,7 @@ def show_stacked_column_chart(request):
     return stacked_column_chart.show_chart()
 
 
-def bar_heat_map(request):
+def show_bar_heat_map(request):
     data = BAR_HEATMAP_DATA
     print(data)
     y_var_names = ["value"]
@@ -215,6 +261,28 @@ def bar_heat_map(request):
     bar_heat_map_chart = XY_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_var_names, y_var_titles,
                                   y_var_units, x_axis_type, y_axis_title, data, color_couple, use_default_colors,
                                   chart_3d, min_max_y_value, 'bar_heat_map_chart')
+
+    return bar_heat_map_chart.show_chart()
+
+
+def show_heat_map_chart(request):
+    data = HEAT_MAP_CHART_DATA
+    y_axis_name = 'hour'
+    y_axis_title = 'Hour'
+    y_axis_unit = '-'
+    x_axis_name = "weekday"
+    x_axis_title = "Weekday"
+    x_axis_unit = "-"
+    z_axis_name = "value"
+    z_axis_title = "Value"
+    z_axis_unit = "m"
+    color_list_request = "cyan"
+    min_max_z_value = [1900, 11000]
+
+    color_list = AM_CHARTS_COLOR_HEATMAP_COUPLES.get(color_list_request, define_color_code_list([color_list_request]))
+    bar_heat_map_chart = XYZ_chart(request, x_axis_name, x_axis_title, x_axis_unit, y_axis_name, y_axis_title,
+                                   y_axis_unit, z_axis_name, z_axis_title, z_axis_unit, data, color_list,
+                                   min_max_z_value, 'heat_map_chart')
 
     return bar_heat_map_chart.show_chart()
 
@@ -250,9 +318,17 @@ def thermometer_chart(request):
     return render(request, 'visualiser/thermometer_chart.html')
 
 
-def define_color_list(color_list_request):
+def define_color_index_list(color_list_request):
     color_list = []
     for color in color_list_request:
         color_list.append(AM_CHARTS_COLOR_INDEX_LIST[color])
+    return color_list
+
+
+def define_color_code_list(color_list_request):
+    color_list = []
+    for color in color_list_request:
+        if not AM_CHARTS_COLOR_CODES_LIST.get(color) is None:
+            color_list.append(AM_CHARTS_COLOR_CODES_LIST[color])
     return color_list
 
