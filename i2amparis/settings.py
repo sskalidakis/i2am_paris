10f5ae5eb3b9e96d8787dfe8fa7b1eee1e2d9cb7
epu_temp_steve <sskalidakis@epu.ntua.gr>
2021-02-09 16:51:12 +0200

    remove debug from public settings

--- i2amparis/settings.py
+++ i2amparis/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
